07d2c256b070834b2cf627ff891acc0f23f191fc
AdrienFerrer <adrien.ferrer@isep.fr>
2019-01-24 19:04:27 +0100

    some profile changes

--- src/Films/FavouriteFilms.js
+++ src/Films/FavouriteFilms.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
-import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
 import InboxIcon from '@material-ui/icons/Inbox';
 import DraftsIcon from '@material-ui/icons/Drafts';
 
@@ -28,41 +27,38 @@ class FavouriteFilms extends Component {
     this.state = {}
   }
 
-  
-
-
-
   render() {
+    const theData = [
+        {
+            name: 'Sam',
+            email: 'somewhere@gmail.com'
+        },
+    
+        {
+            name: 'Ash',
+            email: 'something@gmail.com'
+        }
+    ]
     return (
         <div>
             <br/>
             <h4>Films you loved</h4>
-            <List component="nav">
-                <ListItem button>
-                <ListItemIcon>
-                    <InboxIcon />
-                </ListItemIcon>
-                <ListItemText primary="Inbox" />
-                </ListItem>
-                <ListItem button>
-                <ListItemIcon>
-                    <DraftsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Drafts" />
-                </ListItem>
-            </List>
-            <Divider />
-            <List component="nav">
-                <ListItem button>
-                <ListItemText primary="Trash" />
-                </ListItem>
-                <ListItemLink href="#simple-list">
-                <ListItemText primary="Spam" />
-                </ListItemLink>
-            </List>
+            <Grid container justify="center">
+                <Grid item xs={4}>
+                    <List component="nav">
+                        {theData.map((anObjectMapped, index) => {
+                            return (
+                                <ListItem key={anObjectMapped.name} button>
+                                    <ListItemText primary={anObjectMapped.name+' '+anObjectMapped.email} />
+                                </ListItem>
+                            );
+                        })}
 
+
+                    </List>
+                </Grid>
+            </Grid>
         </div>
-
     );
   }
 }
