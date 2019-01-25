c2911ba2b3cd943774e1c25b5f06e0f7957cee19
Eya ZOUARI <eya.zouari@esprit.tn>
2019-01-25 14:46:27 +0100

    Liaison film et actor

--- src/Films/Film.js
+++ src/Films/Film.js
@@ -30,7 +30,8 @@ render() {
         "description": "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
         "year": 2016,
         "imageUrl": "/images/Kingsglaive_Final_Fantasy_XV.jpg",
-        "rating": 4.5
+        "rating": 4.5,
+		"actorID": 1
     },
     {
         "id": 200,
@@ -39,7 +40,8 @@ render() {
         "description": "A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.",
         "year": 2001,
         "imageUrl": "./images/Final_Fantasy_Spirits_Within.jpg",
-        "rating": 4.5
+        "rating": 4.5,
+		"actorID": 1
     },
     {
         "id": 300,
@@ -48,7 +50,8 @@ render() {
         "description": "A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).",
         "year": 2008,
         "imageUrl": "./images/Ghost_In_The_Shell_2_0.jpg",
-        "rating": 5
+        "rating": 5,
+		"actorID": 1
     },
     {
         "id": 400,
@@ -57,7 +60,8 @@ render() {
         "description": "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope, the legendary city of Olympus.",
         "year": 2014,
         "imageUrl": "./images/Appleseed_Alpha.jpg",
-        "rating": 3.8
+        "rating": 3.8,
+		"actorID": 3
     },
     {
         "id": 500,
@@ -66,14 +70,39 @@ render() {
         "description": "Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.",
         "year": 2014,
         "imageUrl": "./images/Resident_Evil_Vendetta.jpg",
-        "rating": 4.2
+        "rating": 4.2,
+		"actorID": 3
     }]
-	console.log(this.film.length)
+	 const theData = [
+        {
+            name: 'Sam',
+            lastname: 'Uisdvs',
+            id : 1
+        },
+    
+        {
+            name: 'Ash',
+            lastname: 'Paljkacn',
+            id : 3
+        }
+    ]
+	var actor=[]
+
 	    for(let i = 0; i < this.film.length; i++) {
 			
      if (this.film[i].id==this.id)
 	 {
 		 this.obj=this.film[i]
+		 for(let j=0;j< theData.length;j++)
+		 {
+		    if(this.obj.actorID==theData[j].id)
+			{
+			    console.log(this.obj)
+				console.log(theData[j].id)
+				actor=theData[j]
+				
+			}
+		 }
 	 }
     }
   return (
@@ -83,14 +112,15 @@ render() {
         <div>
             <img  src={this.obj.imageUrl} />
             <div>
-                <h4>{this.obj.title}</h4>
-                <h6>{this.obj.subtitle}</h6>
-                <p  style={{fontSize: '14px'}}>{this.obj.description}</p>
+                <h4>Title : {this.obj.title}</h4>
+                <h6>Subtitle : {this.obj.subtitle}</h6>
+                <p  style={{fontSize: '14px'}}>Description : {this.obj.description}</p>
+				<p  style={{fontSize: '14px'}}>Actor : {actor.name+' '+actor.lastname}</p>
             </div>
             <div>
                 <div>
       
-                    <div >{this.obj.rating}
+                    <div >Rating : {this.obj.rating}
 					</div>
                 </div>
             </div>
--- src/Films/listFilms.js
+++ src/Films/listFilms.js
@@ -18,14 +18,15 @@ class ListFilms extends Component {
   }
 
   render() {
-	   this.film =[ {
+	    this.film =[ {
         "id": 100,
         "title": "Kingsglaive",
         "subtitle": "Final Fantasy XV",
         "description": "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
         "year": 2016,
-        "imageUrl": "./images/Kingsglaive_Final_Fantasy_XV.jpg",
-        "rating": 4.5
+        "imageUrl": "/images/Kingsglaive_Final_Fantasy_XV.jpg",
+        "rating": 4.5,
+		"actorID": 1
     },
     {
         "id": 200,
@@ -34,7 +35,8 @@ class ListFilms extends Component {
         "description": "A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.",
         "year": 2001,
         "imageUrl": "./images/Final_Fantasy_Spirits_Within.jpg",
-        "rating": 4.5
+        "rating": 4.5,
+		"actorID": 1
     },
     {
         "id": 300,
@@ -43,7 +45,8 @@ class ListFilms extends Component {
         "description": "A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).",
         "year": 2008,
         "imageUrl": "./images/Ghost_In_The_Shell_2_0.jpg",
-        "rating": 5
+        "rating": 5,
+		"actorID": 1
     },
     {
         "id": 400,
@@ -52,7 +55,8 @@ class ListFilms extends Component {
         "description": "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope, the legendary city of Olympus.",
         "year": 2014,
         "imageUrl": "./images/Appleseed_Alpha.jpg",
-        "rating": 3.8
+        "rating": 3.8,
+		"actorID": 3
     },
     {
         "id": 500,
@@ -61,7 +65,8 @@ class ListFilms extends Component {
         "description": "Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.",
         "year": 2014,
         "imageUrl": "./images/Resident_Evil_Vendetta.jpg",
-        "rating": 4.2
+        "rating": 4.2,
+		"actorID": 3
     }]
 	
 	
